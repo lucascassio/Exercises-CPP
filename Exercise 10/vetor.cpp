dc4e1a1a30d428d780cdca30dd2f12f8ec9df548
Lucas Cassio <99884897+lucascassio@users.noreply.github.com>
2023-06-03 23:56:21 -0300

    Add files via upload

---
+++ Exercise 10/vetor.cpp
@@ -0,0 +1,49 @@
+#include "vetor.h"
+
+using namespace std;
+
+Vetor::Vetor(int inicio, int fim) {
+    if (fim < inicio) {
+        IntervaloVazio x;
+        x.inicio = inicio;
+        x.fim = fim;
+        throw x;
+    }
+    inicio_ = inicio;
+    fim_ = fim;
+    elementos_ = new string[fim - inicio + 1];
+    init_(fim - inicio + 1);
+    fill(init_.begin(), init_.end(), false);
+}
+
+void Vetor::atribuir(int i, std::string valor) {
+    if (i < inicio_ || i > fim_) {
+        IndiceInvalido x;
+        x.indice = i;
+        x.inicio = inicio_;
+        x.fim = fim_;
+        throw x;
+    }
+    elementos_[i - inicio_] = valor;
+    init_[i - inicio_] = true;
+}
+
+string Vetor::valor(int i) const {
+    if (i < inicio_ || i > fim_) {
+        IndiceInvalido x;
+        x.indice = i;
+        x.inicio = inicio_;
+        x.fim = fim_;
+        throw x;
+    }
+    if(init_[i - inicio_] == false) {
+        IndiceNaoInicializado x;
+        x.indice = i;
+        throw x;
+    }
+    return elementos_[i - inicio_];
+}
+
+Vetor::~Vetor() { 
+    delete[] elementos_;
+}
