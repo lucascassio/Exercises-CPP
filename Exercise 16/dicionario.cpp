05fb34d7e3fefd011e121c21351857ed8c270e61
lucascassio <lccosta2604@gmail.com>
2023-06-13 14:17:32 -0300

    vpl 16

--- Exercise 16/dicionario.cpp
+++ Exercise 16/dicionario.cpp
@@ -5,12 +5,7 @@ Dicionario::Dicionario() {
 }
 
 int Dicionario::tamanho() {
-    int aux = 0;
-    for(auto it = elementos_.begin(); it != elementos_.end(); it++) {
-        aux++;
-    }
-    
-    return aux;
+    elementos_.size();
 }
 
 bool Dicionario::pertence(string chave) {
@@ -23,6 +18,12 @@ bool Dicionario::pertence(string chave) {
 }
 
 string Dicionario::menor() {
+
+    if (elementos_.empty()) {
+        DicionarioVazio x;
+        throw x;
+    }
+
     string aux = elementos_.back().chave;
     for(auto it = elementos_.begin(); it != elementos_.end(); it++) {
         if(aux > it -> chave) {
@@ -33,16 +34,30 @@ string Dicionario::menor() {
 }
 
 string Dicionario::valor(string chave) {
+    
     for(auto it = elementos_.begin(); it != elementos_.end(); it++) {
         if(it -> chave == chave) {
             return it -> valor;
         }
     }
+
+    ChaveInexistente x;
+    x.chave = chave;
+    throw x;
 }
 
 void Dicionario::Inserir(string chave, string valor) {
+
+    for(auto it = elementos_.begin(); it != elementos_.end(); it++) {
+        if(it -> chave == chave) {
+            ChaveRepetida x;
+            x.chave = chave;
+            throw x;
+            return;
+        }
+    }
+
     Elemento novo{chave, valor};
-    
     elementos_.push_back(novo);
 }
 
@@ -50,17 +65,26 @@ void Dicionario::Remover(string chave) {
     for(auto it = elementos_.begin(); it != elementos_.end(); it++) {
         if(it -> chave == chave) {
             elementos_.erase(it);
-            break;
+            return;
         }
     }
+
+    ChaveInexistente x;
+    x.chave = chave;
+    throw x;
 }
 
 void Dicionario::Alterar(string chave, string valor) {
     for(auto it = elementos_.begin(); it != elementos_.end(); it++) {
         if(it -> chave == chave) {
             it -> valor = valor;
+            return;
         }
     }
+
+    ChaveInexistente x;
+    x.chave = chave;
+    throw x;
 }
 
 Dicionario::~Dicionario() {
---
+++ Exercise 16/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+#include "dicionario.h"
+
+using namespace std;
+
+int main() {
+  Dicionario m;
+  string operacao, chave, valor;
+  while (cin >> operacao) {
+      if (operacao == "m") {
+        try {
+        cout << m.menor() << endl;
+        } catch(DicionarioVazio e) {
+        cout << "Não foi possível realizar a operação: Dicionário vazio" << endl;
+        }
+      } else if (operacao == "t") {
+        cout << m.tamanho() << endl;
+      } else if (operacao == "p") {
+        cin >> chave;
+        if (m.pertence(chave)) {
+          cout << "sim" << endl;
+        } else {
+          cout << "não" << endl;
+        }
+      } else if (operacao == "v") {
+        try {
+        cin >> chave;
+        cout << m.valor(chave) << endl;
+      } catch(ChaveInexistente e) {
+      cout << "Não foi possível realizar a operação: A chave "  << e.chave << " não está no dicionário" << endl;
+      }
+      } else if (operacao == "i") {
+        cin >> chave >> valor;
+      try {
+      m.Inserir(chave, valor);
+      }
+      catch (ChaveRepetida e) {
+      cout << "Não foi possível realizar a operação: A chave " << e.chave << " já está no dicionário" << endl;
+      }
+      } else if (operacao == "r") {
+      try {
+        cin >> chave;
+        m.Remover(chave);
+      } 
+      catch(ChaveInexistente e) {
+      cout << "Não foi possível realizar a operação: A chave "  << e.chave << " não está no dicionário" << endl;
+      }
+      } else if (operacao == "a") {
+      try {
+        cin >> chave >> valor;
+        m.Alterar(chave, valor);
+      } 
+      catch(ChaveInexistente e) {
+      cout << "Não foi possível realizar a operação: A chave "  << e.chave << " não está no dicionário" << endl;
+     }
+      } else {
+        return 0;
+      }
+  }
+}
+
+
+
